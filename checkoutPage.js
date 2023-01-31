29a87051a7e5ff2ddf2691545f06b14e861329a1
Lgatehouse <98628563+Lgatehouse@users.noreply.github.com>
2023-01-31 10:16:33 -0700

    fixed localStorage?

--- checkoutPage.js
+++ checkoutPage.js
@@ -1,6 +1,8 @@
 var totalPriceHTML = document.getElementById("totalPrice")
 var itemSection = document.getElementById("itemSection")
 
+var totalPrice = 0;
+
 
 window.onload = (event) => {
     var sessionCart = localStorage.getItem('cart');
@@ -13,18 +15,18 @@ window.onload = (event) => {
     itemCount = 0;
     itemSection.innerHTML = ""
 
-    // for (var item of cart) {
-    //     itemCard = `<div id="itemCard">
-    //     <img id = "itemImage" src = "../public/images/items/` + item.name + `.jpg" >
-    //     <p id="itemDescription">` + item.name + `</p>
-    //     <p>` + item.price + `</p>
-    //     <button id="removeFromCart" onclick="removeItemFromCart(` + item.name + `, ${item.price})">Buy</button>
-    //     </div >`
-    //     itemSection.innerHTML += itemCard
-    //     itemCount++;
+    for (var item of cart) {
+        itemCard = `<div id="itemCard">
+        <img id = "itemImage" src = "../public/images/items/` + item.name + `.jpg" >
+        <p id="itemDescription">` + item.name + `</p>
+        <p>` + item.price + `</p>
+        <button id="removeFromCart" onclick="removeItemFromCart(` + item.name + `, ${item.price})">Buy</button>
+        </div >`
+        itemSection.innerHTML += itemCard
+        itemCount++;
 
-    //     totalPrice += item.price
-    // }
+        totalPrice += item.price
+    }
 
     totalPriceHTML.innerHTML = totalPrice
 
