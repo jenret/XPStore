93a1b1450a199257c6688cdcfae0d0a5103c5b6a
choffman4 <choffman@student.neumont.edu>
2023-02-01 09:53:16 -0700

    remove button
    added remove button

--- checkoutPage.js
+++ checkoutPage.js
@@ -2,11 +2,24 @@ var totalPriceHTML = document.getElementById("totalPrice")
 var itemSection = document.getElementById("itemSection")
 
 var totalPrice = 0;
+var cart = []
+
+function removeItemFromCart(itemId) {
+
+    if (itemId > -1) { // only splice array when item is found
+        cart.splice(itemId, 1); // 2nd parameter means remove one item only
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+
+    document.location.reload();
+
+}
 
 
 window.onload = (event) => {
     var sessionCart = localStorage.getItem('cart');
-    var cart = JSON.parse(sessionCart)
+    cart = JSON.parse(sessionCart)
 
     console.log(localStorage.getItem('cart'))
 
@@ -20,15 +33,15 @@ window.onload = (event) => {
         <img id = "itemImage" src = "../public/images/items/` + item.name + `.jpg" >
         <p id="itemDescription">` + item.name + `</p>
         <p>` + item.price + `</p>
-        <button id="removeFromCart" onclick="removeItemFromCart(` + item.name + `, ${item.price})">Remove</button>
+        <button id="removeFromCart" onclick="removeItemFromCart(` + item.id + `)">Remove</button>
         </div >`
         itemSection.innerHTML += itemCard
         itemCount++;
 
         totalPrice += item.price
 
-        
-        
+
+
     }
 
     totalPriceHTML.innerHTML = Math.round(totalPrice * 100) / 100
