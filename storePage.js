c5ff0d31117d8bdf10e8ff57c48b806799859bfe
choffman4 <choffman@student.neumont.edu>
2023-02-06 09:50:44 -0700

    fixed search
    fixed

--- storePage.js
+++ storePage.js
@@ -70,3 +70,23 @@ window.onload = (event) => {
     localStorage.set("itemListSize", itemNameList.length())
 
 }
+
+function searchList() {
+    searchTerm = document.getElementById("searchBar").value
+    itemSection.innerHTML = ""
+    for (var item of itemNameList) {
+        if(item.includes(searchTerm)) {
+            var precision = 100; // 2 decimals
+            var randomPrice = Math.floor(Math.random() * (3 * precision - 1 * precision) + 1 * precision) / (1*precision);
+            itemCard = `<div id="itemCard">
+            <img onclick="addItemToCart(itemNameList[` + itemCount + `], ${randomPrice})" id = "itemImage" src = "../public/images/items/` + item + `.jpg" >
+            <p id="itemDescription">` + item + `</p>
+            <p>$` + randomPrice + `</p>
+            <button id="addToCart" onclick="addItemToCart(itemNameList[` + itemCount + `], ${randomPrice})">Buy</button>
+            </div >`
+            itemSection.innerHTML += itemCard
+            itemCount++;
+        }
+    }
+}
+
